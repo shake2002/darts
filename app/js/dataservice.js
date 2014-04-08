b150983a3fbc8ff439e75be09ef4030423c73db7
Lukasz Kasprzyk <spiler@tlen.pl>
2014-04-08 15:51:00 +0200

    cleanup

--- app/js/dataservice.js
+++ app/js/dataservice.js
@@ -1,39 +1,55 @@
-angular.module('dataSource', [])
+var app = angular.module('dataSource', []);
 
-.factory('fDatService', ['$http', '$rootScope',
-	function($http, $rootScope) {
+app.factory('fDatService', ['$http', '$rootScope', '$q',
+	function ($http, $rootScope, $q) {
 		var modelScope = {
 			players: [],
 			games: []
-		};
-
-		$rootScope.modelScope = modelScope;
-
-		$http.get('/init').success(function(data) {
-			assignToModel(data);
-		});
-		var assignToModel = function(data) {
-			console.log('assign to model: ' + data);
+		},
+		assignToModel = function (data) {
+			console.log('assign to model:  ' + data);
 			modelScope.players = data.players;
 			modelScope.games = data.games;
+		},
+		defer = $q.defer(),
+		postData = function (url, dataToSave, callback) {
+			var defer = $q.defer();
+			$http.post(url, dataToSave).success(function (data) {
+				defer.resolve(data);
+				callback(defer.promise);
+
+			});
+		}, 
+		process = function (promise) {
+			promise.then(function (data) {
+				assignToModel(data);
+			});
 		};
+		$rootScope.modelScope = modelScope;
+
+		$http.get('/init').success(function (data) {
+			defer.resolve(data);
+
+		});
+		process(defer.promise);
 
 		return {
-			addPlayer: function(player) {
-				$http.post('/player/add', player).success(function(data) {
-					assignToModel(data);
-				});
+			addPlayer: function (player) {
+				postData('/player/add', player, process);
 			},
-			removePlayer: function(player) {
-				$http.post('/player/remove', player).success(function(data) {
-					assignToModel(data);
-				});
+			removePlayer: function (player) {
+				postData('/player/remove', player, process);
 			},
-			newGame: function(game) {
-				$http.post('/game/new', game).success(function(data) {
-					assignToModel(data);
-				});
+			newGame: function (game) {
+				postData('/game/new', game, process);
 			}
 		};
 	}
-]);
+	]);
+
+app.service('SaveService', [
+
+	function () {
+
+	}
+	]);
